043018f5b25dcbfec576e69a96fae5f74e488078
teemuatlut <teemu.mantykallio@live.fi>
2018-06-17 22:18:00 +0300

    Update config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -133,7 +133,7 @@
 // The following define selects which electronics board you have.
 // Please choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_14_EFB //BOARD_AZTEEG_X5_GT
+  #define MOTHERBOARD BOARD_RAMPS_14_EFB
 #endif
 
 // Optional custom name for your RepStrap or other custom machine
